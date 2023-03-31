4b895ec80789957452505335262798d0a1195897
Heidi Chen <heidi@macOS-heidi.local>
2023-03-30 21:16:09 -0700

    update video css & js

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,7 +1,13 @@
 export default function decorate(block) {
-    const video = document.createElement('video');
+    // const video = document.createElement('video');
+    // video.src = block.children[0].textContent;
+    // video.controls = true;
+    // video.className = 'video';
+    // block.textContent = '';
+    // block.append(video);
+    const video = document.createElement('iframe');
     video.src = block.children[0].textContent;
-    video.controls = true;
+    video.className = 'video';
     block.textContent = '';
     block.append(video);
   }
